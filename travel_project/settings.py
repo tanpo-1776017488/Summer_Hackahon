7cad97ea0113377e8b7e21bc77e442ef5832dd28
황재현 <tanpo1733@naver.com>
2021-08-11 13:50:29 +0900

    edit settings.py

--- travel_project/settings.py
+++ travel_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY ')
+SECRET_KEY = os.environ.get('SECRET_KEY ','django-insecure-$ivxnqva$l#&w=&zuqx!)or-b1u&tl-h&dts8s4vy@7i#y*^_&')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DEBUG",'True')!='False')
 
